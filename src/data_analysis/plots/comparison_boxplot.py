92f4707de50acde57e3ef787192a383b88b27454
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-04-22 12:46:55 -0300

    Add turning point comparison plots.
    Squashed commit of the following:
    
    commit 33b885fb85449f6dee8a6e41c81ab174b38aafc7
    Author: EstefanoB <48600696+EstefanoB@users.noreply.github.com>
    Date:   Sat Apr 22 12:41:43 2023 -0300
    
        Add plots and no turning point to notebook
    
    commit fb50cbf114349b7d61b3af51f8b04b222570fd5a
    Author: EstefanoB <48600696+EstefanoB@users.noreply.github.com>
    Date:   Sat Apr 22 12:41:08 2023 -0300
    
        Add scatter plot comparing real and permuted
    
    commit f2803d8e191b8be85f2c4b733dc9aff9d71d1ef5
    Author: EstefanoB <48600696+EstefanoB@users.noreply.github.com>
    Date:   Sat Apr 22 12:40:40 2023 -0300
    
        Add boxplot plot comparing real and permuted

---
+++ src/data_analysis/plots/comparison_boxplot.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from turning_point.permutation_coefficient import TurningPointComparison
+
+BOXPLOT_THEME = {
+    "context": "talk",
+    "font_scale": 1.05,
+    "rc": {
+        "figure.figsize": (14, 8),
+        "legend.fontsize": 12,
+        "xtick.labelsize": 14,
+    },
+}
+
+
+def _create_boxplot_df(
+    sport_to_tp_comparison: dict[str, TurningPointComparison], column: str
+) -> pd.DataFrame:
+
+    TITLES = ["Normal", "Permuted"]
+    SECOND_LEVEL_COLUMNS = ["normal", "mean"]
+
+    turning_points_all_sports: list[pd.DataFrame] = []
+
+    for sport, comparison in sport_to_tp_comparison.items():
+
+        col_comparison: pd.DataFrame = comparison.comparison[column]
+        comparison = col_comparison[SECOND_LEVEL_COLUMNS].set_axis(
+            TITLES, axis="columns"
+        )
+
+        for title in TITLES:
+
+            tp_df: pd.DataFrame = comparison[[title]].copy()
+            tp_df[column] = comparison[title]
+            tp_df["Normal/Permuted"] = title
+            tp_df["Sport"] = sport.title()
+
+            turning_points_all_sports.append(tp_df)
+
+    return pd.concat(turning_points_all_sports).drop(TITLES, axis="columns")
+
+
+def plot_comparison_boxplot(
+    sport_to_tp_comparison: dict[str, TurningPointComparison],
+    column: str = "%turning point",
+) -> None:
+
+    boxplot_df = _create_boxplot_df(sport_to_tp_comparison, column)
+
+    with sns.plotting_context(**BOXPLOT_THEME):
+
+        _, ax = plt.subplots()
+
+        sns.boxplot(
+            data=boxplot_df, y="%turning point", x="Sport", hue="Normal/Permuted", ax=ax
+        )
+        ax.set_title("%Date")
+        ax.set_ylabel("")
+        ax.legend(title="")
---
+++ src/data_analysis/plots/comparison_scatter.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import data_analysis.plots.utils.utils as utils
+from turning_point.permutation_coefficient import TurningPointComparison
+
+
+def plot_comparison_scatter(
+    sport_to_tp_comparison: dict[str, TurningPointComparison],
+    nrows: int,
+    ncols: int,
+    figsize: tuple[float, float],
+    column: str = "%turning point",
+) -> None:
+
+    fig, axs = plt.subplots(nrows, ncols, figsize=figsize, sharex="all", sharey="all")
+    flat_axs = [ax for row in axs for ax in row]
+
+    for ax, sport in zip(flat_axs, sport_to_tp_comparison):
+
+        with pd.option_context("mode.use_inf_as_na", True):
+            tp_column = sport_to_tp_comparison[sport].comparison[column]
+
+        not_na = tp_column.dropna()
+        ax.scatter(not_na["normal"], not_na["mean"], alpha=0.3)
+        ax.set_title(sport.title())
+
+        corr_matrix = np.corrcoef(not_na["normal"], not_na["mean"])
+        ax.text(
+            1, 0.2, f"{corr_matrix[0][1]:.2f}", ha="right", va="bottom", fontsize=30
+        )
+
+    utils.add_xlabels_nth_row(fig, axs, "Normal", n=-1)
+    utils.add_ylabels_to_nth_col(fig, axs, "Permuted", n=0)
+
+    fig.subplots_adjust(
+        left=0.11, bottom=0.11, right=0.955, top=0.94, wspace=0.08, hspace=0.15
+    )
