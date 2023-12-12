253c5f950df6205281d135e0bc0281b9f5462a80
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-12-12 13:45:16 -0300

    Fix NaN-values for tournament averages

--- src/data_analysis/numeric_analysis/mean_per_tournament.py
+++ src/data_analysis/numeric_analysis/mean_per_tournament.py
@@ -8,7 +8,12 @@ def get_mean_per_tournament(
     sport_to_comparison: dict[str, pd.DataFrame],
     desired_columns: list[tuple[str, str]],
     sort_by: tuple[str, str] = ("%turning point", "mean"),
+    agg_func: str = "mean",
 ) -> pd.DataFrame:
+    def _apply_agg_func(df: pd.DataFrame) -> pd.Series:
+        """Withough this function, it does not consider NaN values correctly."""
+        return df.agg(agg_func)
+
     all_sports: dict[str, pd.DataFrame] = {}
 
     for sport, comparison in sport_to_comparison.items():
@@ -17,7 +22,10 @@ def get_mean_per_tournament(
         name_country = tps.index.str.extract(NAME_COUNTRY_REGEX)
         new_index = pd.MultiIndex.from_frame(name_country, names=NAME_COUNTRY)
 
-        mean_per_tournament = tps.set_index(new_index).groupby(NAME_COUNTRY).mean()
+        with pd.option_context("mode.use_inf_as_na", True):
+            mean_per_tournament = (
+                tps.set_index(new_index).groupby(NAME_COUNTRY).apply(_apply_agg_func)
+            )
         all_sports[sport] = mean_per_tournament.sort_values(sort_by)
 
     return pd.concat(all_sports, names=["sport"])
