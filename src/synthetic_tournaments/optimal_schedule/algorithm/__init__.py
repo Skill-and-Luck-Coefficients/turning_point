26997dedc6c07cb283461b66f60b7a8c7ee6aa85
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2024-07-19 12:28:29 -0300

    Add graph optimal schedule to init file

--- src/synthetic_tournaments/optimal_schedule/algorithm/__init__.py
+++ src/synthetic_tournaments/optimal_schedule/algorithm/__init__.py
@@ -1,3 +1,4 @@
+from .graph_optimal_schedule import generate_optimal_graph_schedule
 from .recursive_optimal_schedule import (
     generate_optimal_schedule_between_groups,
     generate_recursive_optimal_schedule,
@@ -5,6 +6,7 @@ from .recursive_optimal_schedule import (
 from .types import OptimalFn
 
 __all__ = [
+    "generate_optimal_graph_schedule",
     "generate_recursive_optimal_schedule",
     "generate_optimal_schedule_between_groups",
     "OptimalFn",
