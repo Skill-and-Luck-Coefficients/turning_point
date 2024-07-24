a9462ca8a4ba6261d93407d579a8134b32a6dac1
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2024-07-24 14:39:38 -0300

    Add balance increase analysis

---
+++ src/data_analysis/numeric_analysis/balance_increase.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from turning_point.normal_coefficient import TurningPoint
+
+
+def _get_balance_increase_stats(
+    df: pd.DataFrame,
+    optimal_tp_columns: list[str],
+    tp_column: str = "%turning point",
+) -> pd.DataFrame:
+    tp_df = df[tp_column]
+    increase = tp_df[optimal_tp_columns] - tp_df["normal"].to_frame().to_numpy()
+
+    return pd.DataFrame(
+        {
+            "proportion increase > 0": (increase > 0).mean(),
+            "median increase": (increase.median()),
+            "avg increase": (increase.mean()),
+            "median increase [increase > 0]": (increase[increase > 0].median()),
+            "avg increase [increase > 0]": (increase[increase > 0].mean()),
+            "median decrease [increase < 0]": (increase[increase < 0].abs().median()),
+            "avg decrease [increase < 0]": (increase[increase < 0].abs().mean()),
+        }
+    ).transpose()
+
+
+def get_balance_increase_all_sports(
+    sport_to_tp_df: dict[str, pd.DataFrame],
+    sports: list[str],
+    optimal_cols: list[str],
+    tp_col: str = "%turning point",
+) -> dict[str, pd.DataFrame]:
+    kwargs = {"optimal_tp_columns": optimal_cols, "tp_column": tp_col}
+    return {
+        sport: _get_balance_increase_stats(sport_to_tp_df[sport], **kwargs)
+        for sport in sports
+    }
