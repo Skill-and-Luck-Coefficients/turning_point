5ca8c7481ec35100cc877416f855345840a1d664
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2024-07-24 14:39:49 -0300

    Update analysis init file

--- src/data_analysis/numeric_analysis/__init__.py
+++ src/data_analysis/numeric_analysis/__init__.py
@@ -1,8 +1,10 @@
+from .balance_increase import get_balance_increase_all_sports
 from .bookmaker_predictions import get_sport_to_bookmakers_comparison
 from .bradley_terry_simulation import (
     filter_stats_one_simulation,
     run_bradley_terry_simulations,
 )
+from .bradley_terry_validation import get_validation_summary_all_files
 from .escaping_envelope import get_variance_fluctuating_close_to_envelope
 from .mean_per_tournament import get_mean_per_tournament
 from .metrics_to_final_standings import apply_metrics_to_final_standings
@@ -13,9 +15,11 @@ from .statistical_data import get_statistical_information
 from .temporal_tendency import get_temporal_slope_tendency, style_temporal_tendency
 
 __all__ = [
+    "get_balance_increase_all_sports",
     "get_sport_to_bookmakers_comparison",
     "filter_stats_one_simulation",
     "run_bradley_terry_simulations",
+    "get_validation_summary_all_files",
     "get_variance_fluctuating_close_to_envelope",
     "get_mean_per_tournament",
     "apply_metrics_to_final_standings",
