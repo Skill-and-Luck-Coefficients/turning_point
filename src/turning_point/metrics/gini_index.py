Fix normalization for Gini and HHI

src/turning_point/metrics/gini_index.py
@@ -142,7 +142,7 @@ class NormalizedGini(Metric):
                 ],\n
                 columns=[
                     f"s{i}"-> np.float64
-                        f"s{i}"   : gini index for i-th simulation,
+                        f"s{i}"   : normalized gini index for i-th simulation,
                 ]
             ]
     """
@@ -156,7 +156,7 @@ class NormalizedGini(Metric):
         ppm: PointsPerMatch,
         num_iteration_simulation: tuple[int, int],
         id_to_probabilities: pd.Series | None = None,
-    ) -> Gini:
+    ) -> NormalizedGini:
         """
         Creates an instance from PointsPerMatch.
 
@@ -182,15 +182,21 @@ class NormalizedGini(Metric):
         """
 
         def _calculate_gini_index_per_id(df: pd.DataFrame) -> pd.DataFrame:
-            def _get_upper_bound():
-                _df = build_most_imbalanced_tournament(df)
+            rankings = df.groupby(["id", "team"], observed=True).sum()
+            return rankings.groupby("id", observed=True).apply(gini)
+
+        def _normalization_fn(
+            coef_df: pd.DataFrame, real_ppm: PointsPerMatch
+        ) -> pd.DataFrame:
+
+            def _get_upper_bound() -> pd.Series:
+                _df = build_most_imbalanced_tournament(real_ppm.df)
                 _rankings = _df.groupby(["id", "team"], observed=True).sum()
                 return _rankings.groupby("id", observed=True).apply(gini)
 
-            rankings = df.groupby(["id", "team"], observed=True).sum()
+            upper_bound = _get_upper_bound().to_numpy().reshape(-1, 1)
 
-            gini_index = rankings.groupby("id", observed=True).apply(gini)
-            normalized_gini = gini_index / _get_upper_bound()
+            normalized_gini = coef_df / upper_bound
             return np.clip(normalized_gini, 0, 1)
 
         parameters = get_kwargs_from_points_per_match(
@@ -198,5 +204,6 @@ class NormalizedGini(Metric):
             _calculate_gini_index_per_id,
             num_iteration_simulation,
             id_to_probabilities,
+            _normalization_fn,
         )
         return cls(**parameters)

src/turning_point/metrics/normalized_hhi.py
@@ -95,26 +95,34 @@ class NormalizedHHI(Metric):
                 If None, they will be estimated directly from 'ppm'.
         """
 
-        def _calculate_normalized_hhi_per_id(df: pd.DataFrame) -> pd.DataFrame:
-            def _get_upper_bound():
-                _df = build_most_imbalanced_tournament(df)
+        def _calculate_hhi_per_id(df: pd.DataFrame) -> pd.DataFrame:
+            rankings = df.groupby(["id", "team"], observed=True).sum()
+            return rankings.groupby("id", observed=True).apply(hhi_fn)
+
+        def _normalization_fn(
+            coef_df: pd.DataFrame, real_ppm: PointsPerMatch
+        ) -> pd.DataFrame:
+            def _get_lower_bound():
+                _rankings = real_ppm.df.groupby(["id", "team"], observed=True).sum()
+                return hhi_lower_bound(_rankings)
+
+            def _get_upper_bound() -> pd.Series:
+                _df = build_most_imbalanced_tournament(real_ppm.df)
                 _rankings = _df.groupby(["id", "team"], observed=True).sum()
                 return _rankings.groupby("id", observed=True).apply(hhi_fn)
 
-            rankings = df.groupby(["id", "team"], observed=True).sum()
+            lower_bound = _get_lower_bound().to_numpy().reshape(-1, 1)
+            upper_bound = _get_upper_bound().to_numpy().reshape(-1, 1)
 
-            lower_bound = hhi_lower_bound(rankings)
-            upper_bound = _get_upper_bound()
-
-            hhi = rankings.groupby("id", observed=True).apply(hhi_fn)
-            nhhi = (hhi - lower_bound) / (upper_bound - lower_bound)
+            nhhi = (coef_df - lower_bound) / (upper_bound - lower_bound)
             return np.clip(nhhi, 0, 1)
 
         parameters = get_kwargs_from_points_per_match(
             ppm,
-            _calculate_normalized_hhi_per_id,
+            _calculate_hhi_per_id,
             num_iteration_simulation,
             id_to_probabilities,
+            _normalization_fn,
         )
         return cls(**parameters)
 
@@ -144,7 +152,7 @@ class NaiveNormalizedHHI(Metric):
                 ],\n
                 columns=[
                     f"s{i}"-> np.float64
-                        f"s{i}"   : NormalizedHHI for i-th simulation,
+                        f"s{i}"   : NaiveNormalizedHHI for i-th simulation,
                 ]
             ]
     """
@@ -158,7 +166,7 @@ class NaiveNormalizedHHI(Metric):
         ppm: PointsPerMatch,
         num_iteration_simulation: tuple[int, int],
         id_to_probabilities: pd.Series | None = None,
-    ) -> NormalizedHHI:
+    ) -> NaiveNormalizedHHI:
         """
         Creates an instance from PointsPerMatch.
 
@@ -183,19 +191,27 @@ class NaiveNormalizedHHI(Metric):
                 If None, they will be estimated directly from 'ppm'.
         """
 
-        def _calculate_naive_normalized_hhi_per_id(df: pd.DataFrame) -> pd.DataFrame:
+        def _calculate_hhi_per_id(df: pd.DataFrame) -> pd.DataFrame:
             rankings = df.groupby(["id", "team"], observed=True).sum()
+            return rankings.groupby("id", observed=True).apply(hhi_fn)
 
-            lower_bound = hhi_lower_bound(rankings)
+        def _normalization_fn(
+            coef_df: pd.DataFrame, real_ppm: PointsPerMatch
+        ) -> pd.DataFrame:
+            def _get_lower_bound():
+                _rankings = real_ppm.df.groupby(["id", "team"], observed=True).sum()
+                return hhi_lower_bound(_rankings)
 
-            hhi = rankings.groupby("id", observed=True).apply(hhi_fn)
-            return (hhi - lower_bound) / (1 - lower_bound)
+            lower_bound = _get_lower_bound().to_numpy().reshape(-1, 1)
+            nhhi = (coef_df - lower_bound) / (1 - lower_bound)
+            return np.clip(nhhi, 0, 1)
 
         parameters = get_kwargs_from_points_per_match(
             ppm,
-            _calculate_naive_normalized_hhi_per_id,
+            _calculate_hhi_per_id,
             num_iteration_simulation,
             id_to_probabilities,
+            _normalization_fn,
         )
         return cls(**parameters)
 
@@ -264,18 +280,26 @@ class HICB(Metric):
                 If None, they will be estimated directly from 'ppm'.
         """
 
-        def _calculate_hicb_per_id(df: pd.DataFrame) -> pd.DataFrame:
+        def _calculate_hhi_per_id(df: pd.DataFrame) -> pd.DataFrame:
             rankings = df.groupby(["id", "team"], observed=True).sum()
+            return rankings.groupby("id", observed=True).apply(hhi_fn)
 
-            lower_bound = hhi_lower_bound(rankings)
+        def _normalization_fn(
+            coef_df: pd.DataFrame, real_ppm: PointsPerMatch
+        ) -> pd.DataFrame:
+            def _get_lower_bound():
+                _rankings = real_ppm.df.groupby(["id", "team"], observed=True).sum()
+                return hhi_lower_bound(_rankings)
 
-            hhi = rankings.groupby("id", observed=True).apply(hhi_fn)
-            return hhi / lower_bound
+            lower_bound = _get_lower_bound().to_numpy().reshape(-1, 1)
+            return coef_df / lower_bound
 
         parameters = get_kwargs_from_points_per_match(
             ppm,
-            _calculate_hicb_per_id,
+            _calculate_hhi_per_id,
             num_iteration_simulation,
             id_to_probabilities,
+            _normalization_fn,
         )
+
         return cls(**parameters)

src/turning_point/metrics/top_concentration_ratio.py
@@ -76,16 +76,10 @@ def fast_normalized_top_x_percent_concentration_ratio(
     return normalized_standings[top_index].sum() / normalization
 
 
-def _calculate_normalized_top_x_cr_per_id(df: pd.DataFrame) -> pd.DataFrame:
-    rankings = df.groupby(["id", "team"], observed=True).sum()
-    top_x_cr_fn = fast_normalized_top_x_percent_concentration_ratio
-    return rankings.groupby("id", observed=True).apply(top_x_cr_fn)
-
-
 @dataclass
-class ConcentrationRatio(Metric):
+class NormConcentrationRatio(Metric):
     """
-    Top X% concentration ratio.
+    Normalized top X% concentration ratio.
 
         real:
             pd.DataFrame[
@@ -107,7 +101,7 @@ class ConcentrationRatio(Metric):
                 ],\n
                 columns=[
                     f"s{i}"-> np.float64
-                        f"s{i}"   : top concentration ratio for i-th simulation,
+                        f"s{i}"   : normalized top concentration ratio for i-th simulation,
                 ]
             ]
     """
@@ -121,7 +115,7 @@ class ConcentrationRatio(Metric):
         ppm: PointsPerMatch,
         num_iteration_simulation: tuple[int, int],
         id_to_probabilities: pd.Series | None = None,
-    ) -> ConcentrationRatio:
+    ) -> NormConcentrationRatio:
         """
         Creates an instance from PointsPerMatch.
 
@@ -145,6 +139,89 @@ class ConcentrationRatio(Metric):
 
                 If None, they will be estimated directly from 'ppm'.
         """
+
+        def _calculate_normalized_top_x_cr_per_id(df: pd.DataFrame) -> pd.DataFrame:
+            rankings = df.groupby(["id", "team"], observed=True).sum()
+            top_x_cr_fn = normalized_top_x_percent_concentration_ratio
+            return rankings.groupby("id", observed=True).apply(top_x_cr_fn)
+
+        parameters = get_kwargs_from_points_per_match(
+            ppm,
+            _calculate_normalized_top_x_cr_per_id,
+            num_iteration_simulation,
+            id_to_probabilities,
+        )
+        return cls(**parameters)
+
+
+@dataclass
+class FastNormConcentrationRatio(Metric):
+    """
+    Normalized top X% concentration ratio (fast quantile approximation).
+
+        real:
+            pd.DataFrame[
+                index=[
+                    "id" -> pd.Categorical[str]
+                        "{current_name}@/{sport}/{country}/{name-year}/",
+                ],\n
+                columns=[
+                    "real" -> np.float64
+                        "real": top concentration ratio for real tournaments),\n
+                ]
+            ]
+
+        simulation:
+            pd.DataFrame[
+                index=[
+                    "id" -> pd.Categorical[str]
+                        "{current_name}@/{sport}/{country}/{name-year}/",
+                ],\n
+                columns=[
+                    f"s{i}"-> np.float64
+                        f"s{i}"   : normalized top concentration ratio for i-th simulation,
+                ]
+            ]
+    """
+
+    real: pd.DataFrame
+    simulated: pd.DataFrame
+
+    @classmethod
+    def from_points_per_match(
+        cls,
+        ppm: PointsPerMatch,
+        num_iteration_simulation: tuple[int, int],
+        id_to_probabilities: pd.Series | None = None,
+    ) -> FastNormConcentrationRatio:
+        """
+        Creates an instance from PointsPerMatch.
+
+        -----
+        Parameters:
+
+            points_per_match: PointsPerMatch
+                Points each team gained in each match they played.
+
+            num_iteration_simulation: tuple[int, int]
+                Respectively, number of iterations and number
+                of simulations per iteration (batch size).
+
+            id_to_probabilities: pd.Series | None = None
+                Series mapping each tournament to its estimated probabilities.
+
+                Probabilities:  Mapping[tuple[float, float]: float]
+                    Maps each pair (tuple) to its probability (float).
+
+                    Pair: ranking points gained respectively by home-team and away-team.
+
+                If None, they will be estimated directly from 'ppm'.
+        """
+
+        def _calculate_normalized_top_x_cr_per_id(df: pd.DataFrame) -> pd.DataFrame:
+            rankings = df.groupby(["id", "team"], observed=True).sum()
+            top_x_cr_fn = fast_normalized_top_x_percent_concentration_ratio
+            return rankings.groupby("id", observed=True).apply(top_x_cr_fn)
 
         parameters = get_kwargs_from_points_per_match(
             ppm,