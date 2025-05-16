d8467b26861eb0b7424f338abfbd090d3939fde8
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2025-05-16 20:00:28 -0300

    Add cache to break-minimizer DRR schedules

--- src/synthetic_tournaments/optimal_schedule/scheduling/good_vs_bad_first.py
+++ src/synthetic_tournaments/optimal_schedule/scheduling/good_vs_bad_first.py
@@ -6,9 +6,11 @@ from typing import Sequence
 
 import synthetic_tournaments.break_minimizer.scheduling as min_break
 import tournament_simulations.schedules.round_robin as rr
-from tournament_simulations.schedules import Round
+from tournament_simulations.schedules import Round, rename_teams_in_rounds
 
 from ..algorithm import OptimalFn, generate_recursive_optimal_schedule
+
+MIN_BREAKS_CACHE = {}
 
 
 def create_double_rr(
@@ -53,10 +55,20 @@ def create_break_minimizing_double_rr(
         optimal_schedule = optimal_fn(teams)
         return min_break.min_break_schedule_from_list_schedule(optimal_schedule)
 
-    params = {
-        "team_names": team_names,
-        "num_schedules": num_schedules,
-        "second_portion": second_portion,
-        "optimal_fn": _min_break_scheduling_fn,
-    }
-    return create_double_rr(**params)
+    def _build_schedule_to_cache():
+        params = {
+            "team_names": list(range(num_teams)),
+            "num_schedules": 1,
+            "second_portion": second_portion,
+            "optimal_fn": _min_break_scheduling_fn,
+        }
+        return create_double_rr(**params)
+
+    num_teams = len(team_names)
+    key = f"{optimal_fn.__name__}_{num_teams}"
+
+    if key not in MIN_BREAKS_CACHE:
+        MIN_BREAKS_CACHE[key] = _build_schedule_to_cache()
+
+    schedule = MIN_BREAKS_CACHE[key] * num_schedules
+    return rename_teams_in_rounds(schedule, team_names)
