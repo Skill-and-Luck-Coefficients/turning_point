ffa577a6351cb40c7d21b92f38ae484187861b2e
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2025-05-26 16:10:08 -0300

    Update point system plot function

--- src/data_analysis/plots/new_pontuation.py
+++ src/data_analysis/plots/new_pontuation.py
@@ -12,7 +12,15 @@ def plot_new_pontuation_system(
     sport_to_tp_comparison: dict[str, pd.DataFrame],
     tp_columns: dict[str, str] | dict[tuple[str], str],
     no_difference_margin: float = 0.01,
+    no_x_axis_data: bool = False,
 ):
+
+    def _remove_x_axis_labels_if_necessary():
+        for _ax in axs:
+            if no_x_axis_data:
+                _ax.set_xticklabels([])
+                _ax.set_xlabel("")
+
     ax = axs[0]
     plot_scatter_according_to_line_x_equals_y(
         fig,
@@ -26,8 +34,10 @@ def plot_new_pontuation_system(
         title_as_text_kwargs=None,
     )
     ax.set_title("")
-    ax.set_xticks([0.25, 0.5, 0.75])
+    ax.set_ylim(0, 1)
+    ax.set_xlim(0, 1)
     ax.set_yticks([0, 0.25, 0.5, 0.75])
+    ax.set_xticks([0.25, 0.5, 0.75])
 
     ax = axs[1]
     plot_comparison_boxplot(
@@ -36,4 +46,7 @@ def plot_new_pontuation_system(
 
     ax.set_title("")
     ax.set_yticks([0.25, 0.5, 0.75])
-    ax.set_xticks([-0.20, 0.20], ["3-2 score\nh: 3, a: 0", "3-2 score\nh: 2, a: 1"])
+    xtick_lables = ["3-2 score\n3 winner\n0 loser", "3-2 score\n2 winner\n1 loser"]
+    ax.set_xticks([-0.20, 0.20], xtick_lables)
+
+    _remove_x_axis_labels_if_necessary()
